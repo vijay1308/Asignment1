7179cc2a68b7009dc47d15c85668f75b2c8d8642
Vijay Gupta <vijaygupta@cdnsol.com>
2017-02-25 17:06:40 +0530

    get combination string

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,7 @@
 class AssignmentsController < ApplicationController
 
 	def index
-		@error_message = nil
+		@error_message, @word_combinations = nil,nil
 	  if params[:number].present? && params[:number].length != 10
 	  	  @error_message =  "Enter 10 digit number between 2 to 9 "
 	  	return @error_message   
@@ -9,10 +9,9 @@ class AssignmentsController < ApplicationController
 	  	 @error_message =  "Number should be between 2 to 9"
 	  	return @error_message 
     else
-	 		@print_numbers = PrintNumberWords.get_strings_from_number(params[:number]) if params[:number]	
-    	return @print_numbers
+	 		@combination_string ,@word_combinations = PrintNumberWords.get_strings_from_number(params[:number]) if params[:number]	
     end
-
+   return @combination_string ,@word_combinations
 	end
 
 end
--- app/models/print_number_words.rb
+++ app/models/print_number_words.rb
@@ -7,25 +7,40 @@ class PrintNumberWords
  #  end
 
   def self.get_strings_from_number(number)
-  	digit_letters = {"2" => ['A','B','C'],"3" => ['D','E','F'],"4" => ['G','H','I'],"5" => ['J','K','L'],  	 									"6" => ['M','N','O'],"7" => ['P','Q','R','S'],"8" => ['T','U','V'],"9" => ['W','X','Y','Z']}
+  	digit_letters = {"2" => ['A','B','C'],"3" => ['D','E','F'],"4" => ['G','H','I'],"5" => ['J','K','L'],
+  	  	 									"6" => ['M','N','O'],"7" => ['P','Q','R','S'],"8" => ['T','U','V'],"9" => ['W','X','Y','Z']}
 
     dictionary = []
     file_path = Rails.root.join("dictionary.txt")
     File.foreach( file_path ) do |word|
-      dictionary.push word.to_s
+      dictionary.push word.chop.to_s
     end
     
     puts number
     number_array = number.split('')
     number_letters = number_array.map{|digit|digit_letters[digit]}
-    printcombinations(number_array,number_letters, dictionary)
-   #  #words = keys.shift.product(*keys).map(&:join).reverse
-   #  words = product_of_array(keys)
+    combination_string = printcombinations(number_letters)
+    combination_string = combination_string.select {|word| word.length >=3}
+    return combination_string ,(combination_string & dictionary)
   end
 
 
-  def self.printcombinations(number_array ,number_letters, dictionary)
-  	debugger
+  def self.printcombinations(number_letters)
+	   string, words = [] ,[]
+	    if number_letters.present?
+	      number_letters.each do |letters|
+	        if words.blank?
+	          words = letters
+	        else
+	          string = words.dup
+	          words = words.product(letters).map(&:join)
+	          words.flatten!
+	          words << string
+	          words.flatten!
+	        end
+	      end
+	    end
+    return words
   end
 
 end
