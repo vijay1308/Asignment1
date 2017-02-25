e47db7d324a32577d2a20ccbb2cf357b90870eea
Vijay Gupta <vijaygupta@cdnsol.com>
2017-02-25 13:15:22 +0530

    add interface for enter number

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,8 @@
+class AssignmentsController < ApplicationController
+
+	def index
+	#@print_numbers = PrintNumbers.get_numbers(params[:number]) if params[:number]	
+
+	end
+
+end
---
+++ app/models/print_number.rb
@@ -0,0 +1,13 @@
+class PrintNumbers 
+
+
+	# def initialize(a)
+ #    #@number = "1234567892"
+ #    @number = a
+ #  end
+
+  def get_numbers(a)
+  	puts  a
+  end
+
+end
